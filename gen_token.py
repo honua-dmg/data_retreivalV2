20ad304873a0d5bc33ae1544cfd1d4b58b169988
Guru Sai Prasad Mallepalli Gajula <glavnihonua@gmail.com>
2024-08-13 14:24:22 +0530

    auth_code retreival completely automated

--- gen_token.py
+++ gen_token.py
@@ -87,47 +87,49 @@ class Login():
                 drive = sb.Driver(undetectable=True)
             drive.get(response)"""
             if driver_mode == 0:
-                drive = sb.Driver(uc=True)  # Initialize BaseCase
-                time.sleep(2)
-                drive.open(response)
+                drive = sb.Driver(uc=True,incognito=True)  # Initialize BaseCase
+                #time.sleep(2)
+                drive.uc_open_with_reconnect(response,reconnect_time=5)
             elif driver_mode == 1:
-                drive = sb.Driver(undetectable=True)  # Initialize BaseCase
+                drive = sb.Driver(undetectable=True,incognito=True)  # Initialize BaseCase
                 time.sleep(2)
-                drive.open(response)
-            time.sleep(2)
-            #clicking on phone number box
-            phno = drive.find_element(By.XPATH,'/html/body/section[1]/div[3]/div[3]/form/div[1]/div/input')
-            phno.click()
-            #sending phone number details
-            phno.send_keys(self.phoneno)
+                drive.uc_open_with_reconnect(response,reconnect_time=5)
 
-            #clicking on continue
-            drive.find_element(By.XPATH,'/html/body/section[1]/div[3]/div[3]/form/button').click()
-            #time.sleep(3)
-            #sending TOTP 
+            phno = drive.find_element(By.XPATH,'/html/body/section[1]/div[3]/div[3]/form/div[1]/div/input')
+            drive.uc_click('/html/body/section[1]/div[3]/div[3]/form/div[1]/div/input',By.XPATH)
+            #sending phone number details
+            phno.send_keys('7013036199')
+
+            #click continue 
+            drive.uc_click('/html/body/section[1]/div[3]/div[3]/form/button',By.XPATH)
+            time.sleep(2) #to allow the webpage to load
+
+            #TOTP section
             otp = pyotp.TOTP(self.TOTPseckey).now()
             for i in range(1,7):
                 drive.find_element(By.XPATH,f'/html/body/section[6]/div[3]/div[3]/form/div[3]/input[{i}]').send_keys(otp[i-1])
 
             #pressing continue
-            drive.find_element(By.XPATH,'/html/body/section[6]/div[3]/div[3]/form/button').click()
-            #time.sleep(3)
-            #sending id
+            drive.uc_click('/html/body/section[6]/div[3]/div[3]/form/button',By.XPATH)
+
+            time.sleep(1)
+            # PIN section
             for i in range(1,5):
                 drive.find_element(By.XPATH,f'/html/body/section[8]/div[3]/div[3]/form/div[2]/input[{i}]').send_keys(self.four_digit_key[i-1])
-            drive.find_element(By.XPATH,'/html/body/section[8]/div[3]/div[3]/form/button').click()
+            drive.uc_click('/html/body/section[8]/div[3]/div[3]/form/button',By.XPATH)
+
+            time.sleep(1)
             try:
                 # terms and conditions validation 
-                drive.find_element(By.XPATH,'/html/body/div/div/div/div/div/div[3]/div/div[3]/label').click()
-                drive.find_element(By.XPATH,'/html/body/div/div/div/div/div/div[4]/div/a[2]/span').click()
+                drive.uc_click('/html/body/div/div/div/div/div/div[3]/div/div[3]/label',By.XPATH)
+                drive.uc_click('/html/body/div/div/div/div/div/div[4]/div/a[2]/span',By.XPATH)
             except Exception as e:
                 print('no neeed to validate :)')
             #time.sleep(2)
             url = drive.current_url
             return url.split('&')[2].split('=')[1]
         finally:
-            print('yeassss')
-            #drive.quit()
+            drive.quit()
 
     def get_access_token(self):
         # generating response url if not done manually
